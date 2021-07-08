3c0fc01f3ce2d35d44279f1fae6c3769d523a5f2
lagentcooper <74504470+lagentcooper@users.noreply.github.com>
2021-07-08 11:55:56 +0200

    comit

--- src/Controler/Main.java
+++ src/Controler/Main.java
@@ -10,6 +10,27 @@ package Controler;
  *
  * @author Adrien
  */
+<<<<<<< Updated upstream
 public class Main {
+=======
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/registerPage.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+>>>>>>> Stashed changes
     
 }

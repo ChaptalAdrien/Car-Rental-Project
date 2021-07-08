1acf0d667646e6519985ca49f2c4da49fd3c5c2a
lagentcooper <74504470+lagentcooper@users.noreply.github.com>
2021-07-08 11:22:47 +0200

    clean package

--- src/Controler/Main.java
+++ src/Controler/Main.java
@@ -1,15 +1,37 @@
-package Controler;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Controler;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 /**
  *
- * @author Adrien
+ * @author rouss
  */
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/mainPage.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }

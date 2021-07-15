56a8cd2fa7bd7e203799779fc19a271078640cc5
lagentcooper <74504470+lagentcooper@users.noreply.github.com>
2021-07-15 16:16:34 +0200

    Button

--- src/Controler/ControllerLogReg.java
+++ src/Controler/ControllerLogReg.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Button;
 
 /**
  *

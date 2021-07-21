7eb7a32578f9ba7250d5a0db5aabdf6a2d8896ef
lagentcooper <74504470+lagentcooper@users.noreply.github.com>
2021-07-21 14:55:11 +0200

    link button fxml mainPage

---
+++ src/Controler/MainController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controler;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author rouss
+ */
+public class MainController implements Initializable{
+    
+    @FXML
+    private Button members;
+    @FXML
+    private Button cars;
+    @FXML
+    private Button agencies;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+}

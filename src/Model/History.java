5dc50345e29889c1b9d85a3874742dde58122398
lagentcooper <74504470+lagentcooper@users.noreply.github.com>
2021-07-25 21:50:08 +0200

    History + contructor

---
+++ src/Model/History.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author rouss
+ */
+public class History {
+    
+    private String car;
+    private String type;
+    private String rentDate;
+    private String endDate;
+    private String price;
+    
+    public History(String car, String type, String rentDate, String endDate, String price){
+        this.car = car;
+        this.car = type;
+        this.car = rentDate;
+        this.car = endDate;
+        this.car = price;
+    }
+}
